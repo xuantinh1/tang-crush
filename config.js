fed6b38809d08b91d1e039853f30e01c9b68947d
xuantinh1 <85275395+xuantinh1@users.noreply.github.com>
2021-06-05 01:13:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Em yêu<3 à!',
+    introTitle: 'EM À<3!',
     introDesc: `Trái đất vốn lạ thường,
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,8 +7,8 @@ const CONFIG = {
     btnIntro: 'hihi<3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Có , yêu anh nhiều lắm cơ🥰😘😍<33',
-    btnNo: 'Chọn CÓ đi nha😘:3',
+    btnYes: 'CÓ, HIHI🥰😘😍<33',
+    btnNo: 'KHÔNG,😝😜😘:3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu .<33333333',
