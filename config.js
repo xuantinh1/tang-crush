b10cca7fa5f061ef2346ed96707b0b6e24d4966b
xuantinh1 <85275395+xuantinh1@users.noreply.github.com>
2021-06-04 21:21:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi<3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Có , yêu anh nhiều lắm cơ🥰😘😍<33',
+    btnNo: 'Chọn CÓ đi nha😘:3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/i.am.xuantinh.1708'.
 }
