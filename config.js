2b19d5bbf935e66d68dc23ca18fc7b70ea5ee65e
xuantinh1 <85275395+xuantinh1@users.noreply.github.com>
2021-06-05 01:16:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu .<33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h anh qua đón nhé!😍.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/i.am.xuantinh.1708'.
 }
